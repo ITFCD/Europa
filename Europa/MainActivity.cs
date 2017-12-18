578024bd62bac56d7d470a215e1bbf699a48708c
MarMegh <martinmm@outlook.com>
2017-12-18 14:46:58 -0800

    Add project files.

---
+++ Europa/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Europa
+{
+    [Activity(Label = "Europa", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        DateTime startDate = new DateTime(2018, 1, 16);
+        DateTime today = DateTime.Now;
+        private int calc()
+        {
+            return (startDate - today).Days;
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
